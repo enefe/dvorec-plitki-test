3b09fb3f3e8d4c74d064f8e9e9599ee74dac7f98
enefe815 <enefe815@yandex.ru>
2022-04-27 17:11:58 +0300

    feat: animation

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let button = document.querySelector('.input-check');
 let info = document.querySelector('.collapse-container');
+let border = document.querySelector('.bx-soa-section');
 
 button.onclick = function() {
-    info.classList.toggle('collapse-entered');
+    /* info.classList.toggle('collapse-entered'); */
+    if (info.classList.contains('collapse-entered')) {
+        info.classList.remove('collapse-entered');
+        border.style.border = '1px solid #bd956a';
+    } else {
+        info.classList.add('collapse-entered');
+        border.style.border = '1px solid #e8e8e8';
+    }
 }
